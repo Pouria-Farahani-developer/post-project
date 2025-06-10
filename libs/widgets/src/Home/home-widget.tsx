9c96f74731f5014c6c7b23edb55c642244630a9a
pouria <pouria.farahani.developer@gmail.com>
2025-06-10 23:26:57 +0330

    feat: add widgets lib with home widget

---
+++ libs/widgets/src/Home/home-widget.tsx
@@ -0,0 +1,14 @@
+'use client';
+import {  loadTr } from '@myapp/libs/translation';
+
+import fa from './locale/fa';
+import en from './locale/en';
+import App from './components/app/app';
+
+const HomeWidget: React.FC = () => {
+  loadTr({ en, fa });
+
+  return <App />;
+};
+
+export default HomeWidget;
