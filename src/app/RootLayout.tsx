be83915d4615dbd2d3af8606817d6f6c807a83c5
arman <pouria.farahani.developer@gmail.com>
2025-11-06 14:32:57 +0330

    feat: add appbar to layout

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -8,12 +8,13 @@ import { Layout, Menu, Breadcrumb, Switch, theme } from 'antd';
 import { useTr } from '@myapp/libs/translation';
 import { breadcrumbItems, items } from '../../libs/widgets/src/Home/utils/helper';
 import { useTheme } from '@myapp/libs/ui-kit';
+import { Header } from 'antd/es/layout/layout';
 
 const { Content, Footer, Sider } = Layout;
 
 const MainLayoutChildren = ({ children }: React.PropsWithChildren) => {
   const [t] = useTr();
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
   const { isDarkMode, toggleTheme, toggleLanguage, language } = useTheme();
 
   const {
@@ -22,52 +23,62 @@ const MainLayoutChildren = ({ children }: React.PropsWithChildren) => {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
+      <Header
+        style={{
+          textAlign: 'center',
+          padding: 0,
+        }}
       >
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={['1']}
-          mode="inline"
-          items={items(t)}
-        />
-      </Sider>
+        <h3 style={{ color: 'white', margin: 0 }}>AppBar</h3>
+      </Header>
       <Layout>
-        <Breadcrumb style={{ margin: '16px' }} items={breadcrumbItems} />
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            items={items(t)}
+          />
+        </Sider>
+        <Layout>
+          <Breadcrumb style={{ margin: '16px' }} items={breadcrumbItems} />
 
-        <Content style={{ margin: '0 16px' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            {children}
-            <Switch
-              checked={language !== 'fa_IR'}
-              onChange={() => {
-                toggleLanguage();
+          <Content style={{ margin: '0 16px' }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
               }}
-              style={{ margin: '2rem' }}
-              checkedChildren={'fa'}
-              unCheckedChildren={'en'}
-            />
-            <Switch
-              checked={isDarkMode}
-              onChange={toggleTheme}
-              checkedChildren="☀️"
-              unCheckedChildren="🌙"
-            />
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
+            >
+              {children}
+              <Switch
+                checked={language !== 'fa_IR'}
+                onChange={() => {
+                  toggleLanguage();
+                }}
+                style={{ margin: '2rem' }}
+                checkedChildren={'fa'}
+                unCheckedChildren={'en'}
+              />
+              <Switch
+                checked={isDarkMode}
+                onChange={toggleTheme}
+                checkedChildren="☀️"
+                unCheckedChildren="🌙"
+              />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout> 
       </Layout>
     </Layout>
   );
